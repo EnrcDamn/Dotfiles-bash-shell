7cfc0accda5f2e2384b772d61f9b623c5b39f9ec
EnrcDamn <enrico.damiani10@gmail.com>
2022-08-03 15:22:48 +0200

    First commit

---
+++ .config/powershell/my_profile.ps1
@@ -0,0 +1,38 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# Prompt
+Import-Module posh-git
+oh-my-posh --init --shell pwsh --config "~/AppData/Local/Programs/oh-my-posh/themes/cloud-native-azure.omp.json" | Invoke-Expression
+# $omp_config = Join-Path $PSScriptRoot ".\my_theme.omp.json"
+# oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+# Set-PSReadLineOption -PredictionViewStyle Listview
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Aliases
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias set Set-Variable
+Set-Alias unset Clear-Variable
+Set-Alias grep Select-String
+Set-Alias touch New-Item
+Set-Alias find Get-ChildItem
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Dotfiles-bash-shell
-My PowerShell / Bash config with oh-my-posh
+# My shell dotfiles
+
+Set-up for the Windows PowerShell prompt with [Oh My Posh](https://github.com/JanDeDobbeleer/oh-my-posh) and [Scoop](https://github.com/ScoopInstaller/Scoop).
+
+For a tutorial on how to configure the patched nerd fonts and a UNIX-like transparent / acrylic background, check out the [devaslife](https://www.youtube.com/watch?v=5-aK2_WwrmM) YouTube video.
+
+## Content
+* Powershell .config
+* Bash .config (to be updated)
+
+## How to Use
+
+## PowerShell setup
+
+Scoop installation (for admin installation check out [this](https://github.com/ScoopInstaller/Install#for-admin)):
+
+```iwr -useb get.scoop.sh | iex```
+
+Installing Git:
+
+```winget install -e --id Git.Git```
+
+Installing Neovim with Scoop:
+
+```scoop install neovim gcc```
+
+Setting up the custom profile as the CurrentUserCurrentHost profile (for further details about profile, visit the [Microsoft website](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.2)):
+
+```vim $PROFILE.CurrentUserCurrentHost```
+
+Modify the profile adding this command:
+
+```$env:USERPROFILE\.config\powershell\my_profile.ps1```
+
+## Bash setup
+
+(to be updated)
